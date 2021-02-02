013665cf6092e3cee0071f88c2cb9ddf1aa91f73
Audrey <audreyngai123@gmail.com>
2021-02-01 22:47:56 -0800

    Update Prompts.pg13.js

--- server/state/generated-data/Prompts.pg13.js
+++ server/state/generated-data/Prompts.pg13.js
@@ -3,5 +3,8 @@ export function getRandomPG13Prompt() {
 }
 
 const prompts = [
-  "https://github.com/chubbibunniomnomnom/cursed-emojis/blob/master/emoji1.png"
+  "https://github.com/chubbibunniomnomnom/cursed-emojis/blob/master/emoji1.png",
+  "https://github.com/chubbibunniomnomnom/cursed-emojis/blob/master/emoji2.png",
+  "https://github.com/chubbibunniomnomnom/cursed-emojis/blob/master/emoji3.png",
+  "https://github.com/chubbibunniomnomnom/cursed-emojis/blob/master/emoji4.png"
 ];
