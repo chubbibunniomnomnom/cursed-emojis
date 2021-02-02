3d93b1582aaa7d4e06ed40fa49e6eff72ddddc84
Audrey <audreyngai123@gmail.com>
2021-02-02 01:08:58 -0800

    Update HostsGame.jsx

--- src/pages/host/HostsGame.jsx
+++ src/pages/host/HostsGame.jsx
@@ -10,6 +10,43 @@ import holdPhoneUpImage from "../../images/hold-phone-up.png";
 import chargingPunchImage from "../../images/starting-punch.png";
 import completedPunchImage from "../../images/completed-punch.png";
 import completedStrongPunchImage from "../../images/completed-powerful-punch.png";
+import emoji1 from "../../images/emoji1.png";
+import emoji2 from "../../images/emoji2.png";
+import emoji3 from "../../images/emoji3.png";
+import emoji4 from "../../images/emoji4.png";
+import emoji5 from "../../images/emoji5.png";
+import emoji6 from "../../images/emoji6.png";
+import emoji7 from "../../images/emoji7.png";
+import emoji8 from "../../images/emoji8.png";
+import emoji9 from "../../images/emoji9.png";
+import emoji10 from "../../images/emoji10.png";
+import emoji11 from "../../images/emoji11.png";
+import emoji12 from "../../images/emoji12.png";
+import emoji13 from "../../images/emoji13.png";
+import emoji14 from "../../images/emoji14.png";
+import emoji15 from "../../images/emoji15.png";
+import emoji16 from "../../images/emoji16.png";
+import emoji17 from "../../images/emoji17.png";
+import emoji18 from "../../images/emoji18.png";
+import emoji19 from "../../images/emoji19.png";
+import emoji20 from "../../images/emoji20.png";
+import emoji21 from "../../images/emoji21.png";
+import emoji22 from "../../images/emoji22.png";
+import emoji23 from "../../images/emoji23.png";
+import emoji24 from "../../images/emoji24.png";
+import emoji25 from "../../images/emoji25.png";
+import emoji26 from "../../images/emoji26.png";
+import emoji27 from "../../images/emoji27.png";
+import emoji28 from "../../images/emoji28.png";
+import emoji29 from "../../images/emoji29.png";
+import emoji30 from "../../images/emoji30.png";
+import emoji31 from "../../images/emoji31.png";
+import emoji32 from "../../images/emoji32.png";
+import emoji33 from "../../images/emoji33.png";
+import emoji34 from "../../images/emoji34.png";
+import emoji35 from "../../images/emoji35.png";
+import emoji36 from "../../images/emoji36.png";
+import emoji37 from "../../images/emoji37.png";
 
 class HostsGame extends Component {
   state = {
@@ -212,6 +249,120 @@ class HostsGame extends Component {
     playBackgroundMusic();
   }
 
+  promptToImage(str) {
+    if (str === "emoji1") {
+      return emoji1;
+    }
+    if (str === "emoji2") {
+      return emoji2;
+    }
+    if (str === "emoji3") {
+      return emoji3;
+    }
+    if (str === "emoji4") {
+      return emoji4;
+    }
+    if (str === "emoji5") {
+      return emoji5;
+    }
+    if (str === "emoji6") {
+      return emoji6;
+    }
+    if (str === "emoji7") {
+      return emoji7;
+    }
+    if (str === "emoji8") {
+      return emoji8;
+    }
+    if (str === "emoji9") {
+      return emoji9;
+    }
+    if (str === "emoji10") {
+      return emoji10;
+    }
+    if (str === "emoji11") {
+      return emoji11;
+    }
+    if (str === "emoji12") {
+      return emoji12;
+    }
+    if (str === "emoji13") {
+      return emoji13;
+    }
+    if (str === "emoji14") {
+      return emoji14;
+    }
+    if (str === "emoji15") {
+      return emoji15;
+    }
+    if (str === "emoji16") {
+      return emoji16;
+    }
+    if (str === "emoji17") {
+      return emoji17;
+    }
+    if (str === "emoji18") {
+      return emoji18;
+    }
+    if (str === "emoji19") {
+      return emoji19;
+    }
+    if (str === "emoji20") {
+      return emoji20;
+    }
+    if (str === "emoji21") {
+      return emoji21;
+    }
+    if (str === "emoji22") {
+      return emoji22;
+    }
+    if (str === "emoji23") {
+      return emoji23;
+    }
+    if (str === "emoji24") {
+      return emoji24;
+    }
+    if (str === "emoji25") {
+      return emoji25;
+    }
+    if (str === "emoji26") {
+      return emoji26;
+   }
+    if (str === "emoji27") {
+      return emoji27;
+    }
+   if (str === "emoji28") {
+      return emoji28;
+    }
+   if (str === "emoji29") {
+      return emoji29;
+    }
+   if (str === "emoji30") {
+      return emoji30;
+    }
+   if (str === "emoji31") {
+      return emoji31;
+    }
+   if (str === "emoji32") {
+      return emoji32;
+    }
+   if (str === "emoji33") {
+      return emoji33;
+    }
+   if (str === "emoji34") {
+      return emoji34;
+    }
+   if (str === "emoji35") {
+      return emoji35;
+    }
+   if (str === "emoji36") {
+      return emoji36;
+    }
+   if (str === "emoji37") {
+      return emoji37;
+    }
+  }
+
   onStartNewGameNewPlayersClick() {
     this.props.history.push("/create");
   }
@@ -296,7 +447,7 @@ class HostsGame extends Component {
         let answersCount = 0;
         return (
           <div>
-            <h1 className="prompt" dangerouslySetInnerHTML={{ __html: this.state.prompt }}></h1>
+            <img src={this.promptToImage(this.state.promptsToAnswer[this.state.currentPromptNumber])} />
             <div className="answers">
               {this.state.votingOptions.map((voteOption) => {
                 let cardClasses = "card";
@@ -324,7 +475,7 @@ class HostsGame extends Component {
         let count = 0;
         return (
           <div>
-            <h1 className="prompt" dangerouslySetInnerHTML={{ __html: this.state.prompt }}></h1>
+            <img src={this.promptToImage(this.state.promptsToAnswer[this.state.currentPromptNumber])} />
             <div className="answers">
               {this.state.votingResults.map((voteResult) => {
                 let cardClasses = "card";
